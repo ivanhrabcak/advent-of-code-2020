667143b4c14f8380ade27d80b6157d428d8ac6a9
Ivan Hrabcak <ivan.hrabcak@gmail.com>
2020-12-02 12:19:04 +0100

    add back part 1

---
+++ day 1/part_1.py
@@ -0,0 +1,7 @@
+with open("input.txt", "r") as f:
+    numbers = [int(i) for i in f]
+
+for n in numbers:
+    if 2020 - n in numbers:
+        print(n * numbers[numbers.index(2020 - n)])
+        break
