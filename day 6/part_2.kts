a58b6cddaa9fc681210998482bc8f189ed0b8c96
Ivan Hrabcak <ivan.hrabcak@gmail.com>
2020-12-08 21:31:15 +0100

    why is this not workiiiiing?

--- day 6/part_2.kts
+++ day 6/part_2.kts
@@ -5,19 +5,19 @@ import java.util.function.Consumer
 var data: List<String> = File("input.txt").readText()
         .split("\n\n")
 
-var counter = 0
-for (group in data) {
-    val answers = group.split("\n")
-    for (i in 0 until 26) {
-        val letter = 'a' + i
-        var allContain = true
-        for (answer in answers) {
-            if (!answer.contains(letter)) allContain = false
+var sum = 0
+
+for (i in 0 until 26) {
+    val char: Char = 'a' + i
+    for (group: String in data) {
+        var hasGroupSameAnswers = true
+        for (answer: String in group.split("\n")) {
+            if (answer.contains(char))
+                hasGroupSameAnswers = false
         }
-        if (allContain) {
-            counter++
-        }
+        if (hasGroupSameAnswers)
+            sum++
     }
 }
 
-println(counter)
+println(sum)
